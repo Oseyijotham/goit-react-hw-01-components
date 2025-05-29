880564e497b47116e996c5171e43e8e202841221
Oseyijotham <jothamoboh16@gmail.com>
2025-05-29 08:38:06 +0100

    Update App

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,36 +1,25 @@
 import css from './Profile.module.css';
 
-export const Profile = ({ username, avatar, stats, index }) => (
+export const Profile = ({ username, tag, location, avatar, stats, index }) => (
   <div className={css.profile}>
     <div className={css.description}>
       <img src={avatar} alt="User avatar" className={css.avatar} />
       <p className={css.name}>{username}</p>
+      <p className={css.tag}>@{tag}</p>
+      <p className={css.location}>{location}</p>
     </div>
     <ul className={css.stats}>
       <li key={index} className={css.statsItems}>
-        <span className={css.label}>About Me</span>
-        <span className={css.quantity}>I am a MERN Developer</span>
+        <span className={css.label}>Followers</span>
+        <span className={css.quantity}>{stats.followers.toLocaleString()}</span>
       </li>
       <li key={index} className={css.statsItemsMid}>
-        <span className={css.label}>Skills</span>
-        <span className={css.quantity}>
-          HTML, CSS, React, Node, Express, Docker
-        </span>
+        <span className={css.label}>Views</span>
+        <span className={css.quantity}>{stats.views.toLocaleString()}</span>
       </li>
       <li key={index} className={css.statsItems}>
-        <span className={css.label}>Hobbies and Interests</span>
-        <span className={css.quantity}>Gaming, Reading</span>
-      </li>
-      <li key={index} className={css.statsItems}>
-        <span className={css.label}>Certification</span>
-        <span className={css.quantity}>
-          <a
-            target="_"
-            href="https://drive.google.com/file/d/1c0DyXH2eKTVnOfBhgAAno5X7plUfSgTT/view"
-          >
-            GoIT Fullstack Developer Certification
-          </a>
-        </span>
+        <span className={css.label}>Likes</span>
+        <span className={css.quantity}>{stats.likes.toLocaleString()}</span>
       </li>
     </ul>
   </div>
